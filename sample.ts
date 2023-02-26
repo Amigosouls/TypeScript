be6787e86f0edc6eb6bfa70bd28a8978d8955317
Amigosouls <amigosouls022@gmail.com>
2023-02-26 19:56:38 +0530

    TS 1st commit

---
+++ sample.ts
@@ -0,0 +1,25 @@
+class Employee{
+    empname :string;
+    empid : number;
+    empdesignation : string;
+    empsalary : number;
+    empproject : string;
+
+    public getEmpDetails(empname:string, empid:number, empdesignation:string, empsalary:number, empproject:string) {
+        this.empname = empname;
+        this.empid = empid;
+        this. empdesignation = empdesignation;
+        this.empsalary = empsalary;
+        this.empproject = empproject
+    }
+    public displayEmpDetails(){
+        let result = ["Employee Name:",this.empname,"Employee ID:",this.empid,"Employee Designation:",this.empdesignation,"Employee Salary:",this.empsalary,"Employee Project:",this.empproject];
+        let str = result.toString();
+        document.getElementById("show").innerHTML=str;
+    }
+}
+function showEmployee(){
+    let empObj = new Employee();
+    empObj.getEmpDetails("Arun",23415,"Designer",6000,"HTML");
+    empObj.displayEmpDetails();
+}
