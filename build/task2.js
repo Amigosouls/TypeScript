9812c249a1e28f44da17b07c822b88da28bff976
Amigosouls <amigosouls022@gmail.com>
2023-02-28 16:54:26 +0530

    TS 4th commit

--- build/task2.js
+++ build/task2.js
@@ -1,1 +1,35 @@
 "use strict";
+class Course {
+    constructor(courseId, courseName) {
+        this.courseId = courseId;
+        this.courseName = courseName;
+    }
+    courseDetails() {
+        console.log(`Course Details......\n Course ID:${this.courseId} \n Course Name:${this.courseName} \n`);
+    }
+}
+class Employee extends Course {
+    constructor(courseId, courseName, empId, empName, empDesignation) {
+        super(courseId, courseName);
+        this.empId = empId;
+        this.empName = empName;
+        this.empDesignation = empDesignation;
+    }
+    employeeDetails() {
+        console.log(`Employee Details....\n Employee ID:${this.empId} \n Employee Name:${this.empName} \n Employee Designation:${this.empDesignation}\n`);
+    }
+}
+class Admin extends Employee {
+    constructor(courseId, courseName, empId, empName, empDesignation, adminid, adminName) {
+        super(courseId, courseName, empId, empName, empDesignation);
+        this.adminId = adminid;
+        this.adminName = adminName;
+    }
+    adminDetails() {
+        this.courseDetails();
+        this.employeeDetails();
+        console.log(`Admin Details......\n Admin Id:${this.adminId}\n Admin Name:${this.adminName}`);
+    }
+}
+let obj = new Admin("1356cs52", "UI design", 10, "Peter England", "Employee", "01AD100", "Karthick");
+obj.adminDetails();
---
+++ src/task2.ts
@@ -0,0 +1,42 @@
+class Course{
+    courseId :string | number;
+    courseName:string;
+    constructor(courseId:string|number, courseName:string){
+        this.courseId = courseId;
+        this.courseName=courseName;
+    }
+    public courseDetails():void{
+        console.log(`Course Details......\n Course ID:${this.courseId} \n Course Name:${this.courseName} \n`);
+    }
+}
+
+class Employee extends Course{
+    empId :string|number;
+    empName : string;
+    empDesignation : string;
+    constructor(courseId:string|number, courseName:string,empId :string|number,empName : string,empDesignation : string){
+        super(courseId,courseName)
+        this.empId = empId;
+        this.empName = empName;
+        this.empDesignation = empDesignation;
+    }
+    public employeeDetails():void{
+        console.log(`Employee Details....\n Employee ID:${this.empId} \n Employee Name:${this.empName} \n Employee Designation:${this.empDesignation}\n`);
+    }
+}
+class Admin extends Employee{
+    adminId : number|string;
+    adminName : string;
+    constructor(courseId:string|number, courseName:string,empId :string|number,empName : string,empDesignation : string, adminid:string|number, adminName:string){
+    super(courseId,courseName,empId,empName,empDesignation)
+    this.adminId=adminid;
+    this.adminName=adminName;
+    }
+    public adminDetails():void{
+        this.courseDetails();
+        this.employeeDetails();
+        console.log(`Admin Details......\n Admin Id:${this.adminId}\n Admin Name:${this.adminName}`);
+    }
+}
+let obj = new Admin("1356cs52","UI design",10,"Peter England","Employee","01AD100","Karthick");
+obj.adminDetails();
